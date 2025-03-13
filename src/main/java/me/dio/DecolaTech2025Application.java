d41dcb10453d3c76bd2c8d03ed2caa97277a4721
Caliel Passos <caliel1023@yahoo.com>
2025-03-13 16:04:07 -0300

    Feat: Starting Project

---
+++ src/main/java/me/dio/DecolaTech2025Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecolaTech2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecolaTech2025Application.class, args);
+	}
+
+}
